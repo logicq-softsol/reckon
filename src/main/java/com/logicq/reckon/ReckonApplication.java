ab1c47075a32d945118c058dce4d9c78beba1f2d
sudhanshu lenka <info@logicqsoftsol.com>
2018-12-07 20:28:02 +0530

    changes for reckon

--- src/main/java/com/logicq/reckon/ReckonApplication.java
+++ src/main/java/com/logicq/reckon/ReckonApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.scheduling.annotation.EnableAsync;
